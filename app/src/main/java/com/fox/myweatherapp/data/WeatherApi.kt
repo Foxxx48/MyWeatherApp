0e6e80fecabea923cd13c03137a00d6f6ac04450
Foxxx48 <Foxxdim@mail.ru>
2022-11-10 17:03:29 +0300

    add UI and requests for different cities

--- app/src/main/java/com/fox/myweatherapp/data/WeatherApi.kt
+++ app/src/main/java/com/fox/myweatherapp/data/WeatherApi.kt
@@ -9,6 +9,18 @@ interface WeatherApi {
     @GET("weather?q=Moscow" + RetrofitHelper.API_TOKEN + "&units=metric&lang=ru")
     suspend fun getWeatherInMoscow(): Response<WeatherDataModel>
 
+    @GET("weather?q=London" + RetrofitHelper.API_TOKEN + "&units=metric&lang=ru")
+    suspend fun getWeatherInLondon(): Response<WeatherDataModel>
+
+    @GET("weather?q=Paris" + RetrofitHelper.API_TOKEN + "&units=metric&lang=ru")
+    suspend fun getWeatherInParis(): Response<WeatherDataModel>
+
+    @GET("weather?q=New York" + RetrofitHelper.API_TOKEN + "&units=metric&lang=ru")
+    suspend fun getWeatherInNewYork(): Response<WeatherDataModel>
+
+    @GET("weather?q=Lesnoy Gorodok" + RetrofitHelper.API_TOKEN + "&units=metric&lang=ru")
+    suspend fun getWeatherInLesnoyGorodok(): Response<WeatherDataModel>
+
     @POST("")
     suspend fun saveWeatherData(@Body weatherDataModel: WeatherDataModel)
 
--- app/src/main/java/com/fox/myweatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/fox/myweatherapp/presentation/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.fox.myweatherapp.presentation
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.fox.myweatherapp.data.RetrofitHelper
 import com.fox.myweatherapp.data.WeatherApi
+import com.fox.myweatherapp.data.models.WeatherDataModel
 import com.fox.myweatherapp.databinding.ActivityMainBinding
 import com.fox.myweatherapp.domain.Repository
 import kotlinx.coroutines.CoroutineScope
@@ -13,7 +19,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-
 
 
     private var _binding: ActivityMainBinding? = null
@@ -27,6 +32,8 @@ class MainActivity : AppCompatActivity() {
         ViewModelProvider(this, viewModelFactory)[MainViewModel::class.java]
     }
 
+    var weatherDataModel: WeatherDataModel? = WeatherDataModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
@@ -34,17 +41,52 @@ class MainActivity : AppCompatActivity() {
 
         var weatherApi: WeatherApi = RetrofitHelper.getWeatherApi()
 
-       CoroutineScope(Dispatchers.Main).launch {
-           var list= weatherApi.getWeatherInMoscow()
-           binding.tvCity.text = list.body()?.name?: "None"
-           binding.tvTemperature.text = list.body()?.main?.temp.toString()
-           binding.tvDescription.text = list.body()?.weather?.component1()?.description
-           myLog(list)
 
-       }
+        binding.spinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View, position: Int, id: Long
+            ) {
 
+                CoroutineScope(Dispatchers.Main).launch {
+                    val data = when (position) {
 
-//        viewModel.extractStringFromHtml()
+                        0 -> weatherApi.getWeatherInMoscow()
+                        1 -> weatherApi.getWeatherInLondon()
+                        2 -> weatherApi.getWeatherInParis()
+                        3 -> weatherApi.getWeatherInNewYork()
+                        4 -> weatherApi.getWeatherInLesnoyGorodok()
+                        else -> {
+                            throw RuntimeException("Wrong data")
+                        }
+                    }
+                    weatherDataModel = data.body()
+
+                    setViews()
+
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // write code to perform some action
+            }
+        }
+    }
+
+    private fun setViews() {
+        binding.tvCity.text = weatherDataModel?.name ?: "None"
+        binding.tvTemperature.text = "${weatherDataModel?.main?.temp.toString()}°"
+        binding.tvDescription.text = weatherDataModel?.weather?.component1()?.description
+        binding.tvMaxTemp.text = weatherDataModel?.main?.tempMax.toString()
+        binding.tvMinTemp.text = weatherDataModel?.main?.tempMin.toString()
+        binding.tvPressure.text = weatherDataModel?.main?.pressure.toString()
+        binding.tvHumidity.text = weatherDataModel?.main?.humidity.toString()
+        binding.tvFeelsLike.text = weatherDataModel?.main?.feelsLike.toString()
+
+        Glide.with(this@MainActivity)
+            .load("file:///android_asset/day_icons/ic_02d.png")
+            .into(binding.imageView)
     }
 
 
@@ -56,6 +98,7 @@ class MainActivity : AppCompatActivity() {
     private fun myLog(message: Any?) {
         Log.d(TAG, "$message")
     }
+
     companion object {
         private const val TAG = "myAPP"
     }
